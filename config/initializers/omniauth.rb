decd36231154118c547210495f01749cf39dc1eb
Jahfer Husain <jahfer.husain@shopify.com>
2016-07-08 11:21:46 -0400

    Remove old code example

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :shopify,
-    ShopifyApp.configuration.api_key,
-    ShopifyApp.configuration.secret,
-    redirect_uri: ShopifyApp.configuration.redirect_uri,
-    scope: ShopifyApp.configuration.scope
-end
--- config/initializers/shopify_session_repository.rb
+++
@@ -1,7 +0,0 @@
-if Rails.configuration.cache_classes
-  ShopifyApp::SessionRepository.storage = Shop
-else
-  ActionDispatch::Reloader.to_prepare do
-    ShopifyApp::SessionRepository.storage = Shop
-  end
-end
